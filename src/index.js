d5f24e5660f91f7f789113bd35722e769d51b9ae
Hoàng Kim Minh <93044196+hkm15022001@users.noreply.github.com>
2023-06-11 00:13:46 +0700

    finalx

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-      <Route path={"/"} element={<App />} />
+      <Route path={"/"} element={<Login />} />
         <Route path={"/*"} element={<App />} />
         <Route path={"/login"} element={<Login />} />
         <Route path={"/signup"} element={<Signup />} />
